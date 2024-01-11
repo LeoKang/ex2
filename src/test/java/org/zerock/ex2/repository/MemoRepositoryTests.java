1ff96c043b0acf4d5ac0b8c913fa493adc546e24
manic-066g <xeungjin@nate.com>
2024-01-11 17:30:25 +0900

    ~ 62 page

--- src/test/java/org/zerock/ex2/repository/MemoRepositoryTests.java
+++ src/test/java/org/zerock/ex2/repository/MemoRepositoryTests.java
@@ -58,4 +58,11 @@ public class MemoRepositoryTests {
 
         System.out.println(memoRepository.save(memo));
     }
+
+    @Test
+    public void testDelete() {
+        Long mno = 100L;
+
+        memoRepository.deleteById(mno);
+    }
 }
